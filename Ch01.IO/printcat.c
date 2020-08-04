9af3df3d15684d0b7fc25bde5a197b13456e58ec
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2020-08-05 00:53:06 +0900

    Add files via upload

---
+++ Ch01.IO/printcat.c
@@ -0,0 +1,18 @@
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|  출력하기
+*/
+
+#include <stdio.h>
+
+int main(){
+
+  printf("\\    /\\ \n");
+  printf(" )  ( ') -(야옹)\n");
+  printf("(  /  )\n");
+  printf(" \\(__)|\n");
+
+  return 0;
+}
