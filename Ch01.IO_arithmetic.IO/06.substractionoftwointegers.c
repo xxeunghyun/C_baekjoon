bfb3826c6b9fe78df27e8f86bbefd72e367b539e
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2020-08-14 21:16:40 +0900

    2nd

--- Ch01.IO_arithmetic.IO/06.substractionoftwointegers.c
+++ Ch01.IO_arithmetic.IO/06.substractionoftwointegers.c
@@ -1,5 +1,5 @@
 /*
-두 정수 A,B 입력받은 후 나눗셈값 출력
+두 정수 A,B 입력받은 후 뺄셈값 출력
 단, A는 10미만, B는 0 초과
 
 */
@@ -26,7 +26,7 @@ int main(){
       exit(1);
     }
 
-  printf("A(%d) / B(%d) = %d\n",A,B,A/B);
+  printf("A(%d) - B(%d) = %d\n",A,B,A-B);
 
   return 0;
 }
