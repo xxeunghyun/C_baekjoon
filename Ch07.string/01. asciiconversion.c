b0c05fde68a64cdffe0c69d79831a88706a3a417
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-01-04 19:20:38 +0900

    Add files via upload

---
+++ Ch07.string/01. asciiconversion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  //int num;
+  char ch;
+
+  printf("Input character or integer\n");
+  scanf("%c",&ch);
+
+  printf("\nASCII CODE CONVERSION OF %c IS %d\n",ch,ch);
+  return 0;
+}
